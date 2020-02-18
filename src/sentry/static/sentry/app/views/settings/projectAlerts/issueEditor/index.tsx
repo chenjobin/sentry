f4cd74a124400ce989bb1de9488ead84a2d859aa
Billy Vong <billy@sentry.io>
2020-02-17 16:11:35 -0800

    feat(workflow): Add "Delete Rule" button for Issue alerts
    This adds a "Delete Rule" button for Issue alerts

--- src/sentry/static/sentry/app/views/settings/projectAlerts/issueEditor/index.tsx
+++ src/sentry/static/sentry/app/views/settings/projectAlerts/issueEditor/index.tsx
@@ -21,6 +21,7 @@ import {
 import {getDisplayName} from 'app/utils/environment';
 import {t} from 'app/locale';
 import AsyncView from 'app/views/asyncView';
+import Button from 'app/components/button';
 import Form from 'app/views/settings/components/forms/form';
 import LoadingMask from 'app/components/loadingMask';
 import PanelAlert from 'app/components/panels/panelAlert';
@@ -34,6 +35,7 @@ import space from 'app/styles/space';
 import withOrganization from 'app/utils/withOrganization';
 import withProject from 'app/utils/withProject';
 
+import Confirm from 'app/components/confirm';
 import RuleNodeList from './ruleNodeList';
 
 const FREQUENCY_CHOICES = [
@@ -146,6 +148,35 @@ class IssueRuleEditor extends AsyncView<Props, State> {
         loading: false,
       });
       addErrorMessage(t('An error occurred'));
+    }
+  };
+
+  handleDeleteRule = async () => {
+    const {rule} = this.state;
+    const ruleId = isSavedAlertRule(rule) ? `${rule.id}/` : '';
+    const isNew = !ruleId;
+    const {project, organization} = this.props;
+
+    if (isNew) {
+      return;
+    }
+
+    const endpoint = `/projects/${organization.slug}/${project.slug}/rules/${ruleId}`;
+
+    addLoadingMessage(t('Deleting...'));
+
+    try {
+      const resp = await this.api.requestPromise(endpoint, {
+        method: 'DELETE',
+      });
+
+      addSuccessMessage(t('Deleted alert rule'));
+      browserHistory.replace(recreateRoute('', {...this.props, stepBack: -2}));
+    } catch (err) {
+      this.setState({
+        detailedError: err.responseJSON || {__all__: 'Unknown error'},
+      });
+      addErrorMessage(t('There was a problem deleting the alert'));
     }
   };
 
@@ -262,6 +293,19 @@ class IssueRuleEditor extends AsyncView<Props, State> {
           onSubmit={this.handleSubmit}
           initialData={{...rule, environment, actionMatch, frequency: `${frequency}`}}
           submitLabel={t('Save Rule')}
+          extraButton={
+            <Confirm
+              priority="danger"
+              confirmText={t('Delete Rule')}
+              onConfirm={this.handleDeleteRule}
+              header={t('Delete Rule')}
+              message={t('Are you sure you want to delete this rule?')}
+            >
+              <Button priority="danger" type="button">
+                {t('Delete Rule')}
+              </Button>
+            </Confirm>
+          }
         >
           {this.state.loading && <SemiTransparentLoadingMask />}
           <Panel>
